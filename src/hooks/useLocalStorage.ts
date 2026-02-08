9a7fa3e5c950104f3b0cc100c3bd3e0b957d62b1
serguei9090 <serguei246@gmail.com>
2026-02-07 20:12:15 -0500

    books add

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { isDev } from '../utils/env';
 
 function useLocalStorage<T>(key: string, initialValue: T) {
